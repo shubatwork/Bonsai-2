e4d85429635087bf7da8219e712a5f50ac11725a
shubham.gupta7 <shubham.gupta@emergemarket.com>
2025-01-24 18:40:02 +0530

    HGFHG

--- Bonsai/Services/DataAnalysisService.cs
+++ Bonsai/Services/DataAnalysisService.cs
@@ -16,14 +16,17 @@ namespace Bonsai.Services
             int profit = 0;
             int loss = 0;
             var mode = OrderSide.Buy;
-            while (true)
             {
-                Console.WriteLine("Start /n");
                 var restClient = new KucoinRestClient();
                 restClient.SetApiCredentials(new Kucoin.Net.Objects.KucoinApiCredentials
                     ("6792c43bc0a1b1000135cb65", "25ab9c72-17e6-4951-b7a8-6e2fce9c3026", "test1234"));
 
                 var symbolList = await restClient.FuturesApi.Account.GetPositionsAsync();
+                if (symbolList.Data.Count() < 5)
+                {
+                    profit = 0;
+                    loss = 0;
+                }
                 foreach (var symbol in symbolList.Data.Where(x => x.UnrealizedPnl > .003M))
                 {
                     if (symbol != null && symbol.IsOpen)
@@ -31,20 +34,21 @@ namespace Bonsai.Services
                         var z = await restClient.FuturesApi.Trading.PlaceOrderAsync
                         (symbol.Symbol, Kucoin.Net.Enums.OrderSide.Buy, Kucoin.Net.Enums.NewOrderType.Market, 0, closeOrder: true, marginMode: Kucoin.Net.Enums.FuturesMarginMode.Cross);
                         Console.WriteLine("Closed " + symbol.Symbol + " - " + symbol.UnrealizedPnl);
-                        if (z.Success && symbol.UnrealizedPnl > 0)
-                        {
-                            profit++;
-                        }
-                        if (z.Success && symbol.UnrealizedPnl < 0)
-                        {
-                            loss++;
-                        }
                         continue;
                     }
                 }
-                canBuy = profit >= loss;
-                mode = profit >= loss ? mode : (mode == OrderSide.Buy) ? OrderSide.Sell : OrderSide.Buy;
-                if (canBuy)
+
+                foreach (var symbol in symbolList.Data.Where(x => x.UnrealizedPnlPercentage < -3))
+                {
+                    if (symbol != null && symbol.IsOpen)
+                    {
+                        var result = await restClient.FuturesApi.Trading.PlaceOrderAsync
+                                (symbol.Symbol, mode, Kucoin.Net.Enums.NewOrderType.Market, 25, quantityInQuoteAsset: 1, marginMode: Kucoin.Net.Enums.FuturesMarginMode.Cross);
+                        continue;
+                    }
+                }
+
+                if (true && symbolList.Data.Count() < 100)
                 {
                     var tickerList = await restClient.FuturesApi.ExchangeData.GetTickersAsync();
                     {
@@ -54,23 +58,15 @@ namespace Bonsai.Services
                         {
                             var getPositions = await restClient.FuturesApi.Account.GetPositionAsync(randomSymbol);
                             if (getPositions != null && getPositions.Data.IsOpen)
-                            {
-                                continue;
-                            }
-
-                            var result = await restClient.FuturesApi.Trading.PlaceOrderAsync
-                                (randomSymbol, mode, Kucoin.Net.Enums.NewOrderType.Market, 25, quantityInQuoteAsset: 1, marginMode: Kucoin.Net.Enums.FuturesMarginMode.Cross);
-
-                            if (result.Success)
-                            {
-                                Console.WriteLine(randomSymbol);
-                            }
-
+                                return;
                         }
+
+                        var result = await restClient.FuturesApi.Trading.PlaceOrderAsync
+                            (randomSymbol, mode, Kucoin.Net.Enums.NewOrderType.Market, 25, quantityInQuoteAsset: 1, marginMode: Kucoin.Net.Enums.FuturesMarginMode.Cross);
+
                     }
                 }
             }
         }
-
     }
 }
